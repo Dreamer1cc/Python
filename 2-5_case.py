bf204a66941f19102163aa5de3a55187a666f6e4
Dreamer1cc <dreamer1cc@gmail.com>
2017-02-01 13:20:33 +0200

    Задание 2-5
    2-5 . Знаменитая цитата: найдите известное высказывание,
    которое вам понравилось .
    Выведите текст цитаты с именем автора .
    Результат должен выглядеть примерно так (включая кавычки):
    Albert Einstein once said, "A person who never made a
    mistake never tried anything new."

---
+++ 2-5_case.py
@@ -0,0 +1,12 @@
+#2-5_case
+'''
+2-5 . Знаменитая цитата: найдите известное высказывание, 
+которое вам понравилось . 
+Выведите текст цитаты с именем автора . 
+Результат должен выглядеть примерно так (включая кавычки): 
+Albert Einstein once said, "A person who never made a
+mistake never tried anything new." 
+'''
+
+name = 'Caesar'
+print('Caesar once said, "Veni, vidi, \nVici."')
