0173473848edd910161fc014b4e7a545b587b872
Dreamer1cc <dreamer1cc@gmail.com>
2017-02-13 18:14:44 +0200

    chapter 4

---
+++ Chapter 4/4-2.py
@@ -0,0 +1,12 @@
+'''
+4-2 . Животные: создайте список из трех (и более) животных, обладающих общей характеристикой . 
+Используйте цикл for для вывода названий всех животных . 
+• Измените программу так, чтобы вместо простого названия выводилось сообщение, включающее это название, например «A dog would make a great pet» . 
+• Добавьте в конец программы строку с описанием общего свойства . Например, можно вывести сообщение «Any of these animals would make a great pet!» .
+
+'''
+
+animals = ["dog", "cat", "bat", "batman"]
+for animal in animals:
+	print("A " + animal + " would make a great pet")
+print("Any of these animals would make a great pet!")
