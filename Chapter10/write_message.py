6f47dced4908635e3f4eff95e863b5b6fba1c6bd
Dreamer1cc <dreamer1cc@gmail.com>
2017-02-21 17:55:02 +0200

    chapter 10

--- Chapter10/write_message.py
+++ Chapter10/write_message.py
@@ -2,34 +2,64 @@ def read_logs(filename):
     inputfile = filename
     outputfile = 'log_analitics.txt'
     messages = []
+    apendix = "\n=======================================================\n"
     with open(inputfile) as file_object:
         lines = file_object.readlines()
-    for line in lines:
+    for index, line in enumerate(lines):
         if ("ExitCode: 1") in line:
-            message = "ExitCode: 1. Error in Object files"
-            messages.append(message)
-        else:
-            if "Error messages: 1" in line:
-                messages.append("Error messages: 1")
-            elif "Error messages: 2" in line:
-                messages.append("Error messages: 2")
-            elif "Error messages: 3" in line:
-                messages.append("Error messages: 3")
-            elif "Error messages: 4" in line:
-                messages.append("Error messages: 4")
-            elif "Error messages: 5" in line:
-                messages.append("Error messages: 5")
-            elif "Error messages: 6" in line:
-                messages.append("Error messages: 6")
-            elif "Error messages: 7" in line:
-                messages.append("Error messages: 7")
-            elif "Error messages: 8" in line:
-                messages.append("Error messages: 8")
-            elif "Error messages: 9" in line:
-                messages.append("Error messages: 9")
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 1" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 2" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 3" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 4" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 5" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 6" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 7" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 8" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
+        elif "Error messages: 9" in line:
+            log = lines[index - 10: index + 1]
+            for line in log:
+                messages.append(line)
+            messages.append(apendix)
 
     with open(outputfile, 'w') as file_object: 
         for message in messages:
-            file_object.write("\n" + message + "; ") 
+            file_object.write(message) 
 
-#read_logs("update_log.txt")
+read_logs("update_log.txt")
+
