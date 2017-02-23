7f636fab158e66ee1496dd21e855e66a3b8e4c39
Dreamer1cc <dreamer1cc@gmail.com>
2017-02-23 12:40:15 +0200

    Using Settings in programm

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,19 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():    
-# Инициализирует игру и создает объект экрана.
+ # Инициализирует pygame, settings и объект экрана. 
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
     # Запуск основного цикла игры.
     while True:        
         # Отслеживание событий клавиатуры и мыши.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         # Отображение последнего прорисованного экрана.
         pygame.display.flip() 
 run_game()
