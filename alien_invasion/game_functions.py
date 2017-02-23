f301026d6373b5662406d87017b020edfc001e0d
Dreamer1cc <dreamer1cc@gmail.com>
2017-02-23 13:24:37 +0200

    update screen function

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -7,3 +7,11 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    """обновляет изображения на экране и отображает новый экран."""
+    # При каждом проходе цикла перересовываем экран.
+    screen.feel(ai_settings.bg_color)
+    ship.blitme()
+    # Отображение последнего прорисованного экрана. 
+    pygame.display.flip()
