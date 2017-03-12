6c124ff879d4e6f1835d352f7b87c0fffa83d821
Dreamer1cc <dreamer1cc@gmail.com>
2017-03-12 14:30:55 +0200

    refactoring
    Код парсинга логов для поиска ошибок вынесен в функции

--- WorkProject/read.py
+++ WorkProject/read.py
@@ -1,11 +1,14 @@
 import write as w
+import search_exec as se
+import search_db as db
+
 def read_logs(filename):
     """function return file, that contains only errors from logs"""
     inputfile = filename.replace('"', ' ')
     inputfile = inputfile.strip()
     outputfile = 'log_analysis.txt'
     messages = []
-    apendix = "\n=======================================================\n"
+
     try:
         with open(inputfile) as file_object:
             lines = file_object.readlines()
@@ -13,34 +16,10 @@ def read_logs(filename):
         msg = "Sorry, the file " + inputfile + " does not exist."
         print(msg)
     else:
-        for index, line in enumerate(lines):
-            begin_line = line.find("Executing script:")
-            end_line = line.find("Error messages: ")
-            if begin_line != -1:
-                index_from = index
-            if end_line != -1:
-                index_to = index
-                er_count = int(line[end_line+16:end_line+17])
-                if er_count != 0:
-                    w.write_log_to_buffer(
-                        lines, index_from, index_to, messages, apendix
-                        )
-            begin_line = line.find("Running Db")
-            end_line = line.find("ExitCode: ")
-            if begin_line != -1:
-                index_from = index
-            if end_line != -1:
-                index_to = index
-                er_count = int(line[end_line+10:end_line+11])
-                if er_count != 0:
-                    w.write_log_to_buffer(
-                        lines, index_from, index_to, messages, apendix
-                        )
+        messages = db.search_for_parse_db(lines)
+        messages += se.search_for_parse_exec(lines)
 
         with open(outputfile, 'w') as file_object:
             for message in messages:
                 file_object.write(message) 
         print("Errors wrote into the file " + outputfile)
-
-# read_logs("update_log.txt")
-
