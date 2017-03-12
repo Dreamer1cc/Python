890ac6f778ef437aa17ea726b2a5c30568e6a88d
Dreamer1cc <dreamer1cc@gmail.com>
2017-03-12 14:31:37 +0200

    refactoring
    функция поиска ошибок в Deploy-части

---
+++ WorkProject/search_db.py
@@ -0,0 +1,19 @@
+import write as w
+
+
+def search_for_parse_db(lines):
+    apendix = "\n=======================================================\n"
+    messages = []
+    for index, line in enumerate(lines):
+        begin_line = line.find("Running Db")
+        end_line = line.find("ExitCode: ")
+        if begin_line != -1:
+            index_from = index
+        if end_line != -1:
+            index_to = index
+            er_count = int(line[end_line+10:end_line+11])
+            if er_count != 0:
+                w.write_log_to_buffer(
+                    lines, index_from, index_to, messages, apendix
+                    )
+    return messagess
