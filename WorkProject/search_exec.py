7c64e3097b6781eba072406f364e57e85b761032
Dreamer1cc <dreamer1cc@gmail.com>
2017-03-12 14:31:58 +0200

    refactoring
    функция поиска ошибок в части выполнения скриптов.

---
+++ WorkProject/search_exec.py
@@ -0,0 +1,20 @@
+import write as w
+
+
+def search_for_parse_exec(lines):
+    apendix = "\n=======================================================\n"
+    messages = []
+    for index, line in enumerate(lines):
+        begin_line = line.find("Executing script:")
+        end_line = line.find("Error messages: ")
+        if begin_line != -1:
+            index_from = index
+        if end_line != -1:
+            index_to = index
+            er_count = int(line[end_line+16:end_line+17])
+            if er_count != 0:
+                if er_count != 0:
+                    w.write_log_to_buffer(
+                        lines, index_from, index_to, messages, apendix
+                        )
+    return messages
